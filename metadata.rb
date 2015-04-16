e5b826fb755565cf00cb276ae658b8a1438d56bb
Mike Berman <mike.berman@kizzang.com>
2015-04-16 15:38:52 -0700

    Removed git dependency

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 name             "git_ssh_wrapper"
-maintainer       "Travis Staton"
-maintainer_email "hello@travisstaton.com"
+maintainer       "Mike Berman"
+maintainer_email ""
 license          "Apache 2.0"
 description      "Installs/Configures git_ssh_wrapper"
 version          "0.1.2"
 
-depends "git", ">= 2.5.2"
